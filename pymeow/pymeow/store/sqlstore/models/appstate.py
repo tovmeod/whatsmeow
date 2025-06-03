a42cf3ab082bbc937240e007551a7c490c2b3f0b
Avraham <tovmeod@gmail.com>
2025-06-03 13:21:30 +0300

    start using Model suffix to db models

--- pymeow/pymeow/store/sqlstore/models/appstate.py
+++ pymeow/pymeow/store/sqlstore/models/appstate.py
@@ -2,6 +2,7 @@ from tortoise.models import Model
 from tortoise import fields
 
 
+# todo rename to AppStateSyncKeyModel
 class AppStateSyncKey(Model):
     """App state sync keys"""
     jid = fields.CharField(max_length=255)
@@ -14,6 +15,7 @@ class AppStateSyncKey(Model):
         table = "pymeow_app_state_sync_keys"
         unique_together = (("jid", "key_id"),)
 
+# todo rename to AppStateVersionModel
 class AppStateVersion(Model):
     """App state versions"""
     jid = fields.CharField(max_length=255)
@@ -24,3 +26,17 @@ class AppStateVersion(Model):
     class Meta:
         table = "pymeow_app_state_version"
         unique_together = (("jid", "name"),)
+
+class AppStateMutationMACModel(Model):
+    """App state mutation MAC storage."""
+
+    class Meta:
+        table = "whatsmeow_app_state_mutation_macs"
+        unique_together = (("jid", "name", "index_mac"),)  # Based on Go queries
+
+    id = fields.IntField(pk=True)
+    jid = fields.CharField(max_length=255)
+    name = fields.CharField(max_length=255)
+    version = fields.BigIntField()
+    index_mac = fields.BinaryField()
+    value_mac = fields.BinaryField()
